4a18a1371e0211d04528b16a2390c7bff2d58860
jgregor5 <jgregor5@xtec.cat>
2026-05-20 10:03:30 +0200

    use setup_databases for using test memory dbs

--- conftest.py
+++ conftest.py
@@ -6,7 +6,8 @@ os.environ.setdefault("DJANGO_ALLOW_ASYNC_UNSAFE", "true")
 
 @pytest.fixture(scope="session")
 def django_db_setup(django_test_environment, django_db_blocker):
+    from django.test.utils import setup_databases
+    setup_databases(verbosity=0, interactive=False)
     with django_db_blocker.unblock():
         from django.core.management import call_command
-        call_command("migrate", "--run-syncdb")
         call_command("loaddata", "myapp/fixtures/testdb.json")
