1e16203eb8b597ded4f26ae2b9ea81309c45a2b9
jgregor5 <jgregor5@xtec.cat>
2026-05-20 10:05:09 +0200

    relocate setup_databases inside with

--- conftest.py
+++ conftest.py
@@ -6,8 +6,8 @@ os.environ.setdefault("DJANGO_ALLOW_ASYNC_UNSAFE", "true")
 
 @pytest.fixture(scope="session")
 def django_db_setup(django_test_environment, django_db_blocker):
-    from django.test.utils import setup_databases
-    setup_databases(verbosity=0, interactive=False)
     with django_db_blocker.unblock():
+        from django.test.utils import setup_databases
         from django.core.management import call_command
+        setup_databases(verbosity=0, interactive=False)
         call_command("loaddata", "myapp/fixtures/testdb.json")
